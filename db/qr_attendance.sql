477d77109f0a6fde4bcfd421070ce25058975da1
FDC.KEVIN <fdc.kevinar@gmail.com>
2024-11-07 14:02:22 +0800

    Add files via upload

---
+++ db/qr_attendance.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 07, 2024 at 06:58 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `qr_attendance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `fullname` text NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`, `fullname`, `status`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70', 'Administrator', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `std_no` int(11) DEFAULT NULL,
+  `timein` time DEFAULT NULL,
+  `timeout` datetime DEFAULT NULL,
+  `logdate` date DEFAULT NULL,
+  `status` varchar(20) DEFAULT NULL,
+  `room` varchar(255) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `instructor` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `std_no`, `timein`, `timeout`, `logdate`, `status`, `room`, `subject`, `instructor`) VALUES
+(1, 458655, '07:14:55', '2024-10-28 07:15:09', '2024-10-28', 'Present', 'Room A', 'Mathematics', 'Instructor A'),
+(2, 458655, '07:15:23', '2024-10-28 07:15:29', '2024-10-28', 'Present', 'Room B', 'Science', 'Instructor B'),
+(3, 458655, '07:22:59', '2024-10-28 07:23:07', '2024-10-28', 'Present', 'Room C', 'History', 'Instructor C'),
+(4, 458655, '00:59:15', '2024-10-29 00:59:43', '2024-10-29', 'Present', 'Room A', 'Mathematics', 'Instructor B'),
+(5, 458655, '01:53:19', '2024-10-29 01:53:29', '2024-10-29', 'Present', 'Room A', 'History', 'Instructor A'),
+(6, 458655, '05:56:43', '2024-10-29 05:56:51', '2024-10-29', 'Present', 'Room B', 'Mathematics', 'Instructor B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `room_name` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`id`, `room_name`) VALUES
+(2, 'Room A1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `id` int(11) NOT NULL,
+  `subject_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`id`, `subject_name`) VALUES
+(2, 'History');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `std_no` int(11) DEFAULT NULL,
+  `name` text DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `status` int(11) DEFAULT 1 COMMENT 'active=1;inactive=0',
+  `user_type` int(11) NOT NULL COMMENT 'student-1;teacher-2',
+  `created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `std_no`, `name`, `password`, `status`, `user_type`, `created`) VALUES
+(1, 458655, 'Kevin Arizobal', '202cb962ac59075b964b07152d234b70', 1, 1, '2024-10-28 05:20:30'),
+(2, 1234567, 'Ernesto Gonabo', '202cb962ac59075b964b07152d234b70', 1, 1, '2024-10-30 01:36:01'),
+(6, 246810, 'Boss Kupal', 'c4ca4238a0b923820dcc509a6f75849b', 1, 2, '2024-11-07 02:36:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profile`
+--
+
+CREATE TABLE `user_profile` (
+  `id` int(11) NOT NULL,
+  `std_no` int(11) NOT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `year` int(11) DEFAULT NULL,
+  `course` text DEFAULT NULL,
+  `address` text DEFAULT NULL,
+  `email` text DEFAULT NULL,
+  `image` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+INSERT INTO `user_profile` (`id`, `std_no`, `gender`, `year`, `course`, `address`, `email`, `image`) VALUES
+(1, 458655, NULL, NULL, NULL, NULL, NULL, NULL),
+(2, 123, NULL, NULL, NULL, NULL, NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_profile`
+--
+ALTER TABLE `user_profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
